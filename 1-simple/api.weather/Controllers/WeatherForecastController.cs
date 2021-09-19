628ecf29bf8cd1daed5404f8d6871c5396b52f82
salem84 <giorgio.lasala@gmail.com>
2021-09-19 18:52:07 +0200

    added log

--- 1-simple/api.weather/Controllers/WeatherForecastController.cs
+++ 1-simple/api.weather/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace api.weather.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("Get Request");
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
